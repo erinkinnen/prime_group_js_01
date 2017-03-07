21cda3c1210fff3b54ce2417b56677b04ee05751
Erin Kinnen <erin.kinnen@outlook.com>
2017-03-06 19:46:20 -0600

    processEmployees funciton

--- group_01.js
+++ group_01.js
@@ -6,3 +6,46 @@ var robert = ["Robert", "26835", "66000", 1];
 var mayella = ["Mayella", "89068", "35000", 2];
 
 var employees = [atticus, jem, boo, scout, robert, mayella];
+
+function processEmployees (array) {
+  for(var i = 0; i < array.length; i++){
+    // console.log(array[i]);
+    this.name = array[i][0];
+    console.log(this.name);
+    this.bonus = bonusPercent(array[i][1], array[i][3]);
+    console.log(this.bonus);
+}
+}
+processEmployees(employees);
+//
+function bonusPercent (idNumber, employeeRating) {
+
+var bonusRate = 0;
+
+switch (employeeRating) {
+  case 3:
+    bonusRate = 0.04;
+    break;
+  case 4:
+    bonusRate = 0.06;
+    break;
+  case 5:
+    bonusRate = 0.1;
+    break;
+  default:
+    bonusRate = 0;
+   }
+
+if(idNumber.length < 5) {
+  return (bonusRate + 0.05);
+// } else if (salary > "65000") {
+} else {
+  return bonusRate;
+}
+
+
+}
+
+// var employeeBonusPercent = bonusPercent("652", 4);
+//
+// console.log(employeeBonusPercent);
